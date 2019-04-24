4cbe302d0d538f26f7eeb4634e9e51e7f85a1fd9
Nicholas Law <nick@nickwlaw.com>
2019-04-23 21:35:01 -0400

    started implementation of vendor services. only vendor-list is functional.

--- src/app/service/vendor.service.ts
+++ src/app/service/vendor.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { JsonResponse } from '../model/json-response.class';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Vendor } from '../model/vendor.class';
 
 const url = 'http://localhost:8080/vendors/';
 
@@ -13,6 +14,22 @@ export class VendorService {
 
   list(): Observable<JsonResponse> {
     return this.http.get(url) as Observable<JsonResponse>;
+  }
+
+  create(vendor: Vendor): Observable<JsonResponse> {
+    return this.http.post(url, vendor) as Observable<JsonResponse>;
+  }
+
+  edit(vendor: Vendor): Observable<JsonResponse> {
+    return this.http.put(url, vendor) as Observable<JsonResponse>;
+  }
+
+  get(id: string): Observable<JsonResponse> {
+    return this.http.get(url + id) as Observable<JsonResponse>;
+  }
+
+  delete(id: number): Observable<JsonResponse> {
+    return this.http.delete(url + id) as Observable<JsonResponse>;
   }
 
   constructor(private http: HttpClient) { }
